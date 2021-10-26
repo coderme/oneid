c06c3d04711973f923b30721aff37b5413ec2021
CoderMe <srcii@coderme.com>
2021-10-27 00:16:31 +0200

    Updating: uint64_test.go
    Signed-off-by: CoderMe <srcii@coderme.com>

--- uint64_test.go
+++ uint64_test.go
@@ -252,3 +252,4 @@ func TestUint64ForNonUniqueIdOnDifferentServerIDsAcrossMultipleThreads(t *testin
 
 // TestEnvUint64 calls EnvUint64 with custom env variables
 func TestEnvUint64(t *testing.T) {
+	cleanEnvVars()
