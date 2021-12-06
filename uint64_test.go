6360946423818e9bd8a4ccad1eecf0e9772a689b
CoderMe <srcii@coderme.com>
2021-12-06 06:13:51 +0100

    Updating: uint64_test.go
    Signed-off-by: CoderMe <srcii@coderme.com>

--- uint64_test.go
+++ uint64_test.go
@@ -295,3 +295,4 @@ func TestEnvUint64(t *testing.T) {
 	for _, v := range data {
 		err := os.Setenv(serverIDKey, v.ServerID)
 		if err != nil {
+			log.Fatalln("failed to set env", serverIDKey, "to", v.ServerID)
